f823035bc3dc0ae998ba0a3689c3e28f81aaf79c
jusju <jukka.juslin@haaga-helia.fi>
2022-04-28 12:27:21 +0300

    f

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import { CartesianGrid, LineChart, Line, YAxis, XAxis } from 'recharts';
 import { format, parseISO } from "date-fns";
+import data from "./windspeeds.json" assert { type: "json" };
 
 
 function Todolist() {
-  const data = [{name: 'Page A', uv: 1, aika: "10:00 2022-02-02", amt: 2400}, 
-                {name: 'Page B', uv: 2.2, aika: "10:05 2022-02-03", amt: 2400},
-                {name: 'Page C', uv: 1.9, aika: "10:10 2022-02-04", amt: 2400}
+  const [speeds, setSpeeds] = useState([]);
+  const data = [{name: 'Page A', uv: 1, aika: "10:00 02-02", amt: 2400}, 
+                {name: 'Page B', uv: 2.2, aika: "10:05 02-02", amt: 2400},
+                {name: 'Page C', uv: 1.9, aika: "10:10 02-02", amt: 2400},
+                {name: 'Page D', uv: 0.9, aika: "10:15 02-02", amt: 2400},
+                {name: 'Page E', uv: 2.5, aika: "10:20 02-02", amt: 2400},
+                {name: 'Page F', uv: 1.1, aika: "10:25 02-02", amt: 2400}
                ];
 
 
 
 
+useEffect(() => {
+console.log(data);
+});
+
+
   return(
     <div>
-
-  <LineChart width={400} height={400} data={data}>
+<h1>Tuulen nopeus Perttulassa</h1>
+  <LineChart width={1200} height={500} data={data}>
     <CartesianGrid strokeDasharray="3 3" />
     <YAxis axisLine={true}  tickLine={true} />
 
