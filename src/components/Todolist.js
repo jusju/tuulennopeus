346ec09dfec54b76a294adb579d3f3f6fb5714c4
Juslin Jukka <jusju@haaga-helia.fi>
2022-05-02 14:19:09 +0300

    f

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -7,16 +7,33 @@ export default function Todolist() {
   const [speeds, setSpeeds] = useState([]);
 
   const fetchWinds = () => {
-    fetch("http://localhost:8080/tuulet/hae")
+    console.log("We are in fetch winds!!")
+    fetch("windspeeds.json"
+     , {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+     }
+    )
     .then(response => response.json())
     .then(data => console.log(data))
   }
 
 
+  const data = [{name: 'Page A', uv: 1, aika: "10:00 02-02", amt: 2400}, 
+  {name: 'Page B', uv: 2.2, aika: "10:05 02-02", amt: 2400},
+  {name: 'Page C', uv: 1.9, aika: "10:10 02-02", amt: 2400},
+  {name: 'Page D', uv: 0.9, aika: "10:15 02-02", amt: 2400},
+  {name: 'Page E', uv: 2.5, aika: "10:20 02-02", amt: 2400},
+  {name: 'Page F', uv: 1.1, aika: "10:25 02-02", amt: 2400}
+ ];
+
 
 
 useEffect(() => {
-console.log(data);
+  fetchWinds();
+
 });
 
 
@@ -38,12 +55,4 @@ console.log(data);
   );
 }
 
-
-const data = [{name: 'Page A', uv: 1, aika: "10:00 02-02", amt: 2400}, 
-                {name: 'Page B', uv: 2.2, aika: "10:05 02-02", amt: 2400},
-                {name: 'Page C', uv: 1.9, aika: "10:10 02-02", amt: 2400},
-                {name: 'Page D', uv: 0.9, aika: "10:15 02-02", amt: 2400},
-                {name: 'Page E', uv: 2.5, aika: "10:20 02-02", amt: 2400},
-                {name: 'Page F', uv: 1.1, aika: "10:25 02-02", amt: 2400}
-               ];
 
